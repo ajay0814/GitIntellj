41c14aa089d8551a1370057af3cf6bdce32e4f9a
ajay0814 <ajaychowdary2608@gmail.com>
2025-03-26 10:54:57 +0530

    updatedfile

--- src/fanng/Example.java
+++ src/fanng/Example.java
@@ -3,5 +3,6 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("I am Ajay");
         System.out.println("hello");
+        System.out.println("java class");
     }
 }
